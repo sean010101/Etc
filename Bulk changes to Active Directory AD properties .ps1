53aa55ee3946e5d9a35de357e2f35673756463fd
sean010101 <sean010101@users.noreply.github.com>
2015-01-12 11:02:46 -0800

    Create Bulk changes to Active Directory AD properties .ps1

---
+++ Bulk changes to Active Directory AD properties .ps1
@@ -0,0 +1,12 @@
+### Makes bulk changes to Active Directory AD properties based on user identity
+### Create a .CSV with rows Identity, Title, Department
+
+Import-module ActiveDirectory
+
+$userList = Import-Csv 'C:\UserProperties.CSV'
+
+foreach ($user in $userList){
+   Set-ADUser -Identity $user.Identity -Title $user.Title -Department $user.Department
+   ### Line below is for confirmation feedback, optional
+   Get-ADUser -Identity $user.Identity -Properties Title,Department | ft name,Title,Department
+}
